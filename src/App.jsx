3ecdb7a6a67242ddfbbcf2ce8ed28ae6c0eef95f
CallmeHenry <contact@henrynguyen.ca>
2024-05-22 13:46:44 -0700

    add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
+import WarehouseDetails from './components/WarehouseDetails/WarehouseDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>InStock Application</h1>
+        <WarehouseDetails />
       </header>
     </div>
   );
--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -2,10 +2,13 @@
 Create the Warehouse Details component. The user should also be able to 
 navigate back to the Warehouse List Page from this page. Ensure that it works 
 at and between all breakpoints and is fully responsive without any elements overlapping.
+
+route: /warehouse/:warehouseId
 */
 
 
 import './WarehouseDetails.scss';
+import backArrow from '../../assets/Icons/arrow_back-24px.svg';
 import chevronIcon from '../../assets/Icons/chevron_right-24px.svg';
 import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
 import editIcon from '../../assets/Icons/edit-24px.svg';
@@ -14,13 +17,21 @@ import { Link } from 'react-router-dom';
 export default function WarehouseDetails() {
 
     const id = "placeholder";
+    const wareHouse = {
+        location: "WASHINGTON",
+        address: "address",
+        contactName: "contact name",
+        contactInfo: "contact information"
+    };
 
     return (
         <div className="warehouseDetails">
 
             <div className="warehouseDetails__nav">
-                <img className="nav__back-button" />
-                <h1 className="nav__warehouse">WASHINGTON</h1>
+                <button className="nav__back-button">
+                    <img className="nav__back-button" src={backArrow} />
+                </button>
+                <h1 className="nav__warehouse">{wareHouse.location}</h1>
                 <button className="nav__edit-button">
                     <img className="edit-button__image" src={editIcon} alt="edit icon" />
                     <label className="edit-button__label">Edit</label>
@@ -29,21 +40,22 @@ export default function WarehouseDetails() {
 
             <div className="warehouseDetails__info-container">
                 <div className="info-container__address-container">
-                    <h3 className="address-container__heading">WAREHOUSE ADDRESS</h3>
-                    <p className="address-container__address"></p>
+                    <h3 className="address-container__heading">WAREHOUSE ADDRESS:</h3>
+                    <p className="address-container__address">{wareHouse.address}</p>
                 </div>
-                <div className="warehouseDetails__contact-container">
+                <div className="info-container__contact-container">
                     <div className="contact-container__contact-name-container">
-                        <h3 className="contact-name-container__heading">CONTACT NAME</h3>
-                        <p className="contact-name-container__contact"></p>
+                        <h3 className="contact-name-container__heading">CONTACT NAME:</h3>
+                        <p className="contact-name-container__contact">{wareHouse.contactName}</p>
                     </div>
                     <div className="contact-container__contact-info-container">
-                        <h3 className="contact-info-container__heading">CONTACT INFORMATION</h3>
-                        <p className="contact-info-container__contact-info"></p>
+                        <h3 className="contact-info-container__heading">CONTACT INFORMATION:</h3>
+                        <p className="contact-info-container__contact-info">{wareHouse.contactInfo}</p>
                     </div>
                 </div>
             </div>
 
+{/*
             <table className="warehouseDetails__table">
                 <thead className="table__header-container">
                     <tr className="header-container__row">
@@ -56,14 +68,14 @@ export default function WarehouseDetails() {
                 </thead>
                 <tbody className="table__body-container">
                     <tr className="body-container__row">
-                        {/* <Link key={id} to={`/item-details/${id}`}> */}
-                            <td className="body-container__data-item">
-                                <button className="data__button">
-                                    <p className="data__label">Television</p>
-                                    <img className="data__image" src={chevronIcon} alt="chevron icon" />
-                                </button>
-                            </td>
-                        {/* </Link> */}
+                        <Link key={id} to={`/item-details/${id}`}>
+                        <td className="body-container__data-item">
+                            <button className="data__button">
+                                <p className="data__label">Television</p>
+                                <img className="data__image" src={chevronIcon} alt="chevron icon" />
+                            </button>
+                        </td>
+                         </Link> 
                         <td className="body-container__data-category">Electronics</td>
                         <td className="body-container__data-status">IN STOCK</td>
                         <td className="body-container__data-quantity">500</td>
@@ -78,7 +90,7 @@ export default function WarehouseDetails() {
                     </tr>
                 </tbody>
             </table>
-
+    */}
         </div>
     );
 }
