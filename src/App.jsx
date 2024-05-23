38c7e43b598afa10a9931923bc1816d17883e415
Stefan Reeves <stefanreeves24@gmail.com>
2024-05-23 14:21:54 -0700

    commented out placeholder components that have not yet been defined

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Header from "../src/components/header/header.jsx";
 import Footer from "../src/components/footer/footer.jsx";
 
 import WarehousePage from "./pages/WarehousePage/WarehousePage.jsx";
-import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
-import EditWarehousePage from './pages/EditWarehousePage/EditWarehousePage.jsx';
-import AddWarehousePage from './pages/AddWarehousePage/AddWarehousePage.jsx';
+// import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
+// import EditWarehousePage from './pages/EditWarehousePage/EditWarehousePage.jsx';
+// import AddWarehousePage from './pages/AddWarehousePage/AddWarehousePage.jsx';
 import InventoryPage from './pages/InventoryPage/InventoryPage.jsx';
-import InventoryDetailsPage from './pages/InventoryDetailsPage/InventoryDetailsPage.jsx';
-import EditInventoryPage from './pages/EditInventoryPage/EditInventoryPage.jsx';
-import AddInventoryPage from './pages/AddInventoryPage/AddInventoryPage.jsx';
+// import InventoryDetailsPage from './pages/InventoryDetailsPage/InventoryDetailsPage.jsx';
+// import EditInventoryPage from './pages/EditInventoryPage/EditInventoryPage.jsx';
+// import AddInventoryPage from './pages/AddInventoryPage/AddInventoryPage.jsx';
 
 function App() {
   return (
@@ -20,13 +20,13 @@ function App() {
         <div className="routes-container">
           <Routes>
             <Route path='/' element={<WarehousePage/>} />
-            <Route path='/warehouse/:id' element={<WarehouseDetailsPage />} />
+            {/* <Route path='/warehouse/:id' element={<WarehouseDetailsPage />} />
             <Route path='/warehouse/edit/:id' element={<EditWarehousePage/>} />
-            <Route path='/warehouse/add' element={<AddWarehousePage />} />
+            <Route path='/warehouse/add' element={<AddWarehousePage />} /> */}
             <Route path='/inventory' element={<InventoryPage />} />
-            <Route path='/inventory/:id' element={<InventoryDetailsPage />} />
+            {/* <Route path='/inventory/:id' element={<InventoryDetailsPage />} />
             <Route path='/inventory/edit/:id' element={<EditInventoryPage />} />
-            <Route path='/inventory/add' element={<AddInventoryPage/>} />
+            <Route path='/inventory/add' element={<AddInventoryPage/>} /> */}
           </Routes>
         </div>
         <Footer />
