69be2a8458ec4efb0a2b7bcd073b711231407370
ergaganjit <er.gaganjit.kaur@gmail.com>
2024-05-23 15:16:07 -0700

    linked warehouse delete model to the warehouse list page

--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
@@ -1,82 +1,28 @@
-// src/components/DeleteWarehouseModal.js
+// src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Modal from 'react-modal';
 import './DeleteWarehouseModal.scss';
 
-const DeleteWarehouseModal = () => {
-  const [warehouses, setWarehouses] = useState([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [warehouseToDelete, setWarehouseToDelete] = useState(null);
-
-  useEffect(() => {
-    fetchWarehouses();
-  }, []);
-
-  const fetchWarehouses = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/api/warehouses');
-      console.log("response is", response.data);
-      setWarehouses(response.data);
-    } catch (error) {
-      console.error('Error fetching warehouses', error);
-    }
-  };
-
-  const openModal = (warehouse) => {
-    setWarehouseToDelete(warehouse);
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setWarehouseToDelete(null);
-  };
-
-  const deleteWarehouse = async () => {
-    try {
-      await axios.delete(`http://localhost:8080/api/warehouses/${warehouseToDelete.id}`);
-      setWarehouses(warehouses.filter(wh => wh.id !== warehouseToDelete.id));
-      closeModal();
-    } catch (error) {
-      console.error('Error in deleting warehouse', error);
-    }
-  };
-
+const DeleteWarehouseModal = ({ isOpen, onRequestClose, warehouseToDelete, deleteWarehouse }) => {
   return (
-    <div className="warehouse-list">
-      <h1>Warehouses</h1>
-      <ul>
-        {warehouses.map(warehouse => (
-          <li key={warehouse.id}>
-            {warehouse.warehouse_name} <br /> 
-            {warehouse.address}<br />
-            {warehouse.city}<br />
-            {warehouse.country}
-            <button onClick={() => openModal(warehouse)}>🗑️</button>
-          </li>
-        ))}
-      </ul>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Confirm Delete"
-        className="modal"
-        overlayClassName="overlay" // Added overlay class
-      >
-        <div className="modal-header">
-          <h2>Delete {warehouseToDelete?.warehouse_name} Warehouse?</h2>
-          <button className="close-button" onClick={closeModal}>X</button> {/* Close button */}
-        </div>
-        <p>Please confirm that you'd like to delete the {warehouseToDelete?.warehouse_name} warehouse from the list of warehouses. You won't be able to undo this action.</p>
-        <div className="modal-buttons">
-          <button onClick={closeModal}>Cancel</button>
-          <button onClick={deleteWarehouse}>Delete</button>
-        </div>
-      </Modal>
-    </div>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Confirm Delete"
+      className="modal"
+      overlayClassName="overlay"
+    >
+      <div className="modal-header">
+        <h2>Delete {warehouseToDelete?.warehouse_name} Warehouse?</h2>
+        <button className="close-button" onClick={onRequestClose}>X</button>
+      </div>
+      <p>Please confirm that you'd like to delete the {warehouseToDelete?.warehouse_name} warehouse from the list of warehouses. You won't be able to undo this action.</p>
+      <div className="modal-buttons">
+        <button onClick={onRequestClose}>Cancel</button>
+        <button onClick={deleteWarehouse}>Delete</button>
+      </div>
+    </Modal>
   );
 };
 
