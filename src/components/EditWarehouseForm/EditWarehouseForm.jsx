6692e11d3a3ef6cd4572a93e23da369455a4dde7
CallmeHenry <contact@henrynguyen.ca>
2024-05-24 16:48:17 -0700

    add styling for form validation

--- src/components/EditWarehouseForm/EditWarehouseForm.jsx
+++ src/components/EditWarehouseForm/EditWarehouseForm.jsx
@@ -41,84 +41,137 @@ export default function EditWarehouseForm() {
         }
     }
 
-    // TODO: add error handling for empty strings; phone and email validation; and do not send id
     function handleChange(e, key) {
-        // e.preventDefault();
-        // if (e.target.value.trim() === "") {
-            
+        e.preventDefault();
+
+        console.log(e.target.classList);
+        if (e.target.value === "") {
+            e.target.classList.add(`${e.target.className}--error`);
+        } else {
+            e.target.className = `${e.target.classList[0]}`;
+        }
+
+        // if (key === "email" && !isValidEmail(e.target.value)) {
+        //     console.error("Invalid email address");
+        //     e.target.classList.add(`${e.target.className}--error`);
+        //     return;
+        // } else {
+        //     e.target.className = `${e.target.classList[0]}`;
         // }
+
+        // if (key === "phone" && !isValidPhoneNumber(e.target.value)) {
+        //     console.error("Invalid phone number");
+        //     e.target.classList.add(`${e.target.className}--error`);
+        //     return;
+        // } else {
+        //     e.target.className = `${e.target.classList[0]}`;
+        // }
+
         setWareHouseDetails({
             ...wareHouseDetails,
             [key]: e.target.value
         });
     }
 
-return (
-    <div className="editWarehouseForm">
+    // function isValidEmail(email) {
+    //     return /\S+@\S+\.\S+/.test(email);
+    // }
 
-        <div className="editWarehouseForm__nav-container">
-            <div className="editWarehouseForm__nav">
-                <button className="nav__back-button">
-                    <img className="back-button__image" src={backArrow} />
-                </button>
-                <h2 className="nav__heading">Edit Warehouse</h2>
+    // function isValidPhoneNumber(phone) {
+    //     return /^\d{10}$/.test(phone);
+    // }
+
+    return (
+        <div className="editWarehouseForm">
+
+            <div className="editWarehouseForm__nav-container">
+                <div className="editWarehouseForm__nav">
+                    <button className="nav__back-button">
+                        <img className="back-button__image" src={backArrow} />
+                    </button>
+                    <h2 className="nav__heading">Edit Warehouse</h2>
+                </div>
+            </div>
+            <div className="editWarehouseForm__details-container">
+
+                <div className="editWarehouseForm__warehouse-details">
+                    <h3 className="warehouse-details__heading">Warehouse Details</h3>
+
+                    <div className="warehouse-details__warehouse-name">
+                        <label className="warehouse-name__label">Warehouse Name</label>
+                        <input className="warehouse-name__input"
+                            type="text"
+                            value={wareHouseDetails?.warehouse_name}
+                            onChange={(e) => handleChange(e, "warehouse_name")}
+                            required></input>
+                    </div>
+
+                    <div className="warehouse-details__street-address">
+                        <label className="street-address__label">Street Address</label>
+                        <input className="street-address__input"
+                            type="text"
+                            value={wareHouseDetails?.address}
+                            onChange={(e) => handleChange(e, "address")}></input>
+                    </div>
+
+                    <div className="warehouse-details__city">
+                        <label className="city__label">City</label>
+                        <input className="city__input"
+                            type="text"
+                            value={wareHouseDetails?.city}
+                            onChange={(e) => handleChange(e, "city")}></input>
+                    </div>
+
+                    <div className="warehouse-details__country">
+                        <label className="country__label">Country</label>
+                        <input className="country__input"
+                            type="text"
+                            value={wareHouseDetails?.country}
+                            onChange={(e) => handleChange(e, "country")}></input>
+                    </div>
+                </div>
+
+                <div className="editWarehouseForm__contact-details">
+                    <h3 className="contact-details__heading">Contact Details</h3>
+
+                    <div className="contact-details__contact-name">
+                        <label className="contact-name__label">Contact Name</label>
+                        <input className="contact-name__input"
+                            type="text"
+                            value={wareHouseDetails?.contact_name}
+                            onChange={(e) => handleChange(e, "contact_name")}></input>
+                    </div>
+
+                    <div className="contact-details__position">
+                        <label className="position__label">Position</label>
+                        <input className="position__input"
+                            type="text"
+                            value={wareHouseDetails?.contact_position}
+                            onChange={(e) => handleChange(e, "contact_position")}></input>
+                    </div>
+
+                    <div className="contact-details__phone-number">
+                        <label className="phone-number__label">Phone Number</label>
+                        <input className="phone-number__input"
+                            type="tel"
+                            value={wareHouseDetails?.contact_phone}
+                            onChange={(e) => handleChange(e, "contact_phone")}></input>
+                    </div>
+
+                    <div className="contact-details__email">
+                        <label className="email__label">Email</label>
+                        <input className="email__input"
+                            type="email"
+                            value={wareHouseDetails?.contact_email}
+                            onChange={(e) => handleChange(e, "contact_email")}></input>
+                    </div>
+                </div>
+            </div>
+
+            <div className="editWarehouseForm__form-buttons">
+                <button className="form-buttons__cancel-button" onClick={() => handleClick("cancel")}>Cancel</button>
+                <button className="form-buttons__save-button" onClick={() => handleClick("save")}>Save</button>
             </div>
         </div>
-<div className="editWarehouseForm__details-container">
-    
-            <div className="editWarehouseForm__warehouse-details">
-                <h3 className="warehouse-details__heading">Warehouse Details</h3>
-    
-                <div className="warehouse-details__warehouse-name">
-                    <label className="warehouse-name__label">Warehouse Name</label>
-                    <input type="text" className="warehouse-name__input" value={wareHouseDetails?.warehouse_name} onChange={(e) => handleChange(e, "warehouse_name")}></input>
-                </div>
-    
-                <div className="warehouse-details__street-address">
-                    <label className="street-address__label">Street Address</label>
-                    <input className="street-address__input" value={wareHouseDetails?.address} onChange={(e) => handleChange(e, "address")}></input>
-                </div>
-    
-                <div className="warehouse-details__city">
-                    <label className="city__label">City</label>
-                    <input className="city__input" value={wareHouseDetails?.city} onChange={(e) => handleChange(e, "city")}></input>
-                </div>
-    
-                <div className="warehouse-details__country">
-                    <label className="country__label">Country</label>
-                    <input className="country__input" value={wareHouseDetails?.country} onChange={(e) => handleChange(e, "country")}></input>
-                </div>
-            </div>
-    
-            <div className="editWarehouseForm__contact-details">
-                <h3 className="contact-details__heading">Contact Details</h3>
-    
-                <div className="contact-details__contact-name">
-                    <label className="contact-name__label">Contact Name</label>
-                    <input className="contact-name__input" value={wareHouseDetails?.contact_name} onChange={(e) => handleChange(e, "contact_name")}></input>
-                </div>
-    
-                <div className="contact-details__position">
-                    <label className="position__label">Position</label>
-                    <input className="position__input" value={wareHouseDetails?.contact_position} onChange={(e) => handleChange(e, "contact_position")}></input>
-                </div>
-    
-                <div className="contact-details__phone-number">
-                    <label className="phone-number__label">Phone Number</label>
-                    <input className="phone-number__input" value={wareHouseDetails?.contact_phone} onChange={(e) => handleChange(e, "contact_phone")}></input>
-                </div>
-    
-                <div className="contact-details__email">
-                    <label className="email__label">Email</label>
-                    <input className="email__input" value={wareHouseDetails?.contact_email} onChange={(e) => handleChange(e, "contact_email")}></input>
-                </div>
-            </div>
-</div>
-
-        <div className="editWarehouseForm__form-buttons">
-            <button className="form-buttons__cancel-button" onClick={() => handleClick("cancel")}>Cancel</button>
-            <button className="form-buttons__save-button" onClick={() => handleClick("save")}>Save</button>
-        </div>
-    </div>
-);
+    );
 }
