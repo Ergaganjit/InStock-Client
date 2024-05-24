dce92700688856f4f17018b220d7f54dcc44abe7
CallmeHenry <contact@henrynguyen.ca>
2024-05-24 13:30:18 -0700

    modify URL path for edit button

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -37,7 +37,7 @@ export default function WarehouseDetails() {
                         <img className="back-button__image" src={backArrow} onClick={() => navigate(`/`)} />
                     </button>
                     <h1 className="nav__warehouse-heading">{wareHouseDetails?.warehouse_name}</h1>
-                    <Link className="nav__edit-link" to={`/${wareHouseDetails?.id}/edit`}>
+                    <Link className="nav__edit-link" to={`/warehouse/${wareHouseDetails?.id}/edit`}>
                         <button className="edit-link__edit-button">
                             <img className="edit-button__image" src={editIcon} alt="edit icon" />
                             <label className="edit-button__label">Edit</label>
