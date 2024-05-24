fbd5111bcf36e96a901a7cc08ab645162b0ac0f4
dlndn <65935030+dlndn@users.noreply.github.com>
2024-05-23 22:50:00 -0700

    styled inventory header for mobile

--- src/components/WarehouseInventory/WarehouseInventory.jsx
+++ src/components/WarehouseInventory/WarehouseInventory.jsx
@@ -2,6 +2,7 @@ import './WarehouseInventory.scss';
 import { Link } from 'react-router-dom';
 import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
 import editIcon from '../../assets/Icons/edit-24px.svg';
+import searchIcon from '../../assets/Icons/search-24px.svg';
 
 function WarehouseInventory() {
     function InventoryListMobile() {
@@ -21,28 +22,40 @@ function WarehouseInventory() {
                         </div>
                         <div className="inventory-entry__col inventory-entry__col--status">
                             <p className="inventory-entry__label">STATUS</p>
-                            <p className="inventory-entry__col-data inventory-entry__col-data--status">IN STOCK</p>
+                            <p className="inventory-entry__col-data inventory-entry__col-data--status">
+                                IN STOCK
+                            </p>
                         </div>
                     </div>
                     <div className="inventory-entry__row inventory-entry__row--category-qty">
                         <div className="inventory-entry__col inventory-entry__col--category">
                             <p className="inventory-entry__label">CATEGORY</p>
-                            <p className="inventory-entry__col-data inventory-entry__col-data--category">Electronics</p>
+                            <p className="inventory-entry__col-data inventory-entry__col-data--category">
+                                Electronics
+                            </p>
                         </div>
                         <div className="inventory-entry__col inventory-entry__col--qty">
                             <p className="inventory-entry__label">QTY</p>
-                            <p className="inventory-entry__col-data inventory-entry__col-data--qty">500</p>
+                            <p className="inventory-entry__col-data inventory-entry__col-data--qty">
+                                500
+                            </p>
                         </div>
                     </div>
                     <div className="inventory-entry__row inventory-entry__row--spacer-warehouse">
                         <div className="inventory-entry__col inventory-entry__col--spacer"></div>
                         <div className="inventory-entry__col inventory-entry__col--warehouse">
                             <p className="inventory-entry__label">WAREHOUSE</p>
-                            <p className="inventory-entry__col-data">Manhattan</p>
+                            <p className="inventory-entry__col-data">
+                                Manhattan
+                            </p>
                         </div>
                     </div>
                     <div className="inventory-entry__row inventory-entry__row--actions">
-                        <img src={deleteIcon} alt="" className="delete-button" />
+                        <img
+                            src={deleteIcon}
+                            alt=""
+                            className="delete-button"
+                        />
                         <img src={editIcon} alt="" className="edit-button" />
                     </div>
                 </article>
@@ -107,12 +120,20 @@ function WarehouseInventory() {
             <div className="warehouse-inventory">
                 <section className="inventory-header">
                     <h1 className="inventory-header__title">Inventory</h1>
-                    <input
-                        type="text"
-                        name="inventory-search"
-                        class="inventory-search"
-                        className="inventory-header__search-input"
-                    />
+                    <div className="search">
+                        <input
+                            type="text"
+                            name="inventory-search"
+                            id="inventory-search"
+                            className="search__input"
+                            placeholder="Search..."
+                        />
+                        <img
+                            src={searchIcon}
+                            alt="search icon"
+                            className="search__icon"
+                        />
+                    </div>
                     <button className="inventory-header__add-button">
                         + Add New Item
                     </button>
