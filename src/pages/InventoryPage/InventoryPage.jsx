fbdd12eac7fe49fcf533468f9aeb35fe14825c47
dlndn <65935030+dlndn@users.noreply.github.com>
2024-05-26 18:36:12 -0700

    changed structure of InventoryPage and added its styling file

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import WarehouseInventory from '../../components/WarehouseInventory/WarehouseInventory';
+import './InventoryPage.scss';
 
 function InventoryPage() {
-  return (<>
-    <h2>Inventory Page</h2>
-    <WarehouseInventory />
-  </>);
+  return (
+    <section className="inventory-page">
+        <div className="inventory-page__content">
+            <WarehouseInventory />
+        </div>
+    </section>
+  );
 }
 
 export default InventoryPage;
